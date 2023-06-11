cc5a15dc6d57956132cb44ef65d87113be03d698
dom <mannadomenico2849@gmail.com>
2023-06-11 18:57:25 -0400

    Implement logout

--- clientApp/src/components/authenticatedLayout/header.tsx
+++ clientApp/src/components/authenticatedLayout/header.tsx
@@ -3,14 +3,27 @@ import Toolbar from '@mui/material/Toolbar';
 import { Container, Link as MuiLink } from '@mui/material';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { routePaths } from '../../constants/routePaths';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import { usersApi } from '../../api';
 
 const Header = () => {
+  const { clearUserInfo } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    clearUserInfo();
+    await usersApi.logout();
+  };
+
   return (
     <AppBar position="static" sx={{ marginBottom: '1rem' }}>
       <Container disableGutters>
-        <Toolbar sx={{ justifyContent: 'flex-end' }} variant="dense">
+        <Toolbar sx={{ justifyContent: 'flex-end', gap: '1rem' }} variant="dense">
           <MuiLink component={ReactRouterLink} to={routePaths.home} sx={{ color: '#fff' }}>
             Home
+          </MuiLink>
+          <MuiLink component={ReactRouterLink} to={routePaths.login} sx={{ color: '#fff' }} onClick={handleLogout}>
+            Logout
           </MuiLink>
         </Toolbar>
       </Container>
